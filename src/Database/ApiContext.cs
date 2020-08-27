f29e53032146744a51067859fff300655a882017
Swerik <derswerik@gmail.com>
2020-08-27 15:38:49 +0200

    cleanup

--- src/Database/ApiContext.cs
+++ src/Database/ApiContext.cs
@@ -7,10 +7,7 @@ namespace API.Database
 {
     public class ApiContext : DbContext
     {
-        public ApiContext(DbContextOptions<ApiContext> options)
-            : base(options)
-        {
-        }
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
     }
