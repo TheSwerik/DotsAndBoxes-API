f8fd5af1fb0248425fdc16db0f60e26c21c46ac7
Swerik <derswerik@gmail.com>
2020-08-27 14:21:37 +0200

    add TODO

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -57,6 +57,7 @@ namespace API.Controllers
         [HttpPost("authenticate")]
         public async Task<IActionResult> Authenticate([FromBody] AuthenticateModel model)
         {
+            //TODO make HttpGet
             var user = await _userService.Authenticate(model.Username, model.Password);
             if (user == null) return Unauthorized(new {message = "Username or password is incorrect"});
             return Ok(user);
